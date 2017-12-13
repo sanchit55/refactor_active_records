838c2a7fbffc2d894ada0a6fc2f483253818126f
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-12-13 18:12:17 -0500

    adding todos

---
+++ todo.php
@@ -0,0 +1,16 @@
+<?php
+class todo extends model {
+    public $id;
+    public $owneremail;
+    public $ownerid;
+    public $createddate;
+    public $duedate;
+    public $message;
+    public $isdone;
+    protected static $modelName = 'todo';
+    public static function getTablename(){
+        $tableName='todos';
+        return $tableName;
+    }
+}
+?>
